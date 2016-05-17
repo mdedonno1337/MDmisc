6942a0658a2a1a1289a68493017c2ad941dcabb5
Marco De Donno <Marco.DeDonno@unil.ch>
2016-05-17 22:58:38 +0200

    Get a sorted list by key indexes

--- listSort.py
+++ listSort.py
@@ -4,3 +4,6 @@ def argsort( seq ):
     # http://stackoverflow.com/questions/3382352/equivalent-of-numpy-argsort-in-basic-python/3382369#3382369
     # by unutbu
     return sorted( range( len( seq ) ), key = seq.__getitem__ )
+
+def getByIndexes( lst, keys ):
+    return [ lst[i] for i in keys ]
