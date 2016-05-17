5efbb05afdd68ae374d49125d2fe71c2d30d65a6
Marco De Donno <Marco.DeDonno@unil.ch>
2016-05-17 22:42:39 +0200

    python numpy.argsort equivalent to get the index of the sorted values of a list

---
+++ argsort.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+def argsort( seq ):
+    # http://stackoverflow.com/questions/3382352/equivalent-of-numpy-argsort-in-basic-python/3382369#3382369
+    # by unutbu
+    return sorted( range( len( seq ) ), key = seq.__getitem__ )
