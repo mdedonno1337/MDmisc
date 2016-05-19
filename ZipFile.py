ea8787d10bdf3823ec32d901459aa29808c72898
Marco De Donno <Marco.DeDonno@unil.ch>
2016-05-19 20:39:32 +0200

    Extension of the 'zipfile' package, allowing to remove and save a file to disk directly from a ZipFile object

---
+++ ZipFile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#  *-* coding: cp850 *-*
+
+import zipfile
+from cStringIO import StringIO as io
+
+class ZipFileExtended( zipfile.ZipFile ):
+    def removeFile( self, *filenames ):
+        outbuff = io()
+        
+        try:
+            with zipfile.ZipFile( self.fp, 'r' ) as zipread:
+                with zipfile.ZipFile( outbuff, 'w' ) as zipwrite:
+                    for item in zipread.infolist():
+                        if item.filename not in filenames:
+                            data = zipread.read( item.filename )
+                            zipwrite.writestr( item, data )
+            
+            self.fp = outbuff
+            
+            self.filelist = []
+            self._RealGetContents()
+            
+        except Exception as e:
+            print str( e )
+            
+        finally:
+            return
+            
+    def saveToFile( self, outputfile = None ):
+        if outputfile == None:
+            outputfile = self.filename
+        
+        with open( outputfile, "wb+" ) as fp:
+            fp.write( self.fp.getvalue() )
+    
