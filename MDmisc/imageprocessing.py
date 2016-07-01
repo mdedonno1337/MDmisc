9d2276645b427e76059189fed4ccabeeb36947ab
Marco De Donno <Marco.DeDonno@unil.ch>
2016-07-01 20:37:50 +0200

    Add the parameter res to the RAWToPil function. The resolution is set in the PIL object if passed in parameter.

--- MDmisc/imageprocessing.py
+++ MDmisc/imageprocessing.py
@@ -9,14 +9,20 @@ from PIL import Image
 # 
 ################################################################################
 
-def RAWToPIL( raw, size = ( 500, 500 ) ):
+def RAWToPIL( raw, size = ( 500, 500 ), res = None ):
     """
-        Convert a RAW string to PIL object.
+        Convert a RAW string to PIL object. If the resolution is passed in
+        argument, the PIL object resolution is set.
         
         >>> RAWToPIL( chr( 255 ) * 250000, ( 500, 500 ) ) #doctest: +ELLIPSIS
         <PIL.Image.Image image mode=L size=500x500 at 0x...>
     """
-    return Image.frombytes( 'L', size, raw )
+    img = Image.frombytes( 'L', size, raw )
+    
+    if res != None:
+        img.info[ 'dpi' ] = ( res, res )
+        
+    return img
 
 def PILToRAW( pil ):
     """
