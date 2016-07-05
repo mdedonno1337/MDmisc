125ba15c3db0909b919dc830c23309f93c4cd8c7
Marco De Donno <Marco.DeDonno@unil.ch>
2016-07-05 22:54:51 +0200

    add the replace_r for recursive replacement.

---
+++ MDmisc/elist.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from .map_r import map_r
+
+def replace_r( x, s, r ):
+    def test( x, s = s, r = r ):
+        if x == s:
+            return r
+        else:
+            return x
+    
+    return map_r( test, x )
