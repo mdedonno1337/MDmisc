0649508cac75fb7a7c939bca975a8ef4ece55b93
Marco De Donno <Marco.DeDonno@unil.ch>
2016-07-05 02:20:04 +0200

    add the function get_by_keys in the defaultdict type

--- MDmisc/RecursiveDefaultDict.py
+++ MDmisc/RecursiveDefaultDict.py
@@ -5,6 +5,9 @@ from _collections import defaultdict
 
 from MDmisc.DefaultOrderedDict import DefaultOrderedDict
 
+class edefaultdict( defaultdict ):
+    def get_by_keys( self, lst ):
+        return [ self.get( key ) for key in lst ]
 
-defDict = lambda: defaultdict( defDict )
+defDict = lambda: edefaultdict( defDict )
 defOrderedDict = lambda: DefaultOrderedDict( defOrderedDict )
