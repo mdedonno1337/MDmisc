ce2ba7067a9a0b3b0e09aa92e063ab7ca3f0dc4b
Marco De Donno <Marco.DeDonno@unil.ch>
2016-07-06 16:25:07 +0200

    Doctest on binary.py

--- doctester.py
+++ doctester.py
@@ -4,12 +4,15 @@
 import doctest
 import unittest
 
+from MDmisc import binary
 from MDmisc import string
 from MDmisc import xfrange
+
 
 def MDmisctsets():
     tests = unittest.TestSuite()
     
+    tests.addTests( doctest.DocTestSuite( binary ) )
     tests.addTests( doctest.DocTestSuite( string ) )
     tests.addTests( doctest.DocTestSuite( xfrange ) )
     
@@ -17,6 +20,7 @@ def MDmisctsets():
 
 if __name__ == "__main__":
     unittest.TextTestRunner( verbosity = 2 ).run( MDmisctsets() )
+    
 else:
     def load_tests( loader, tests, ignore ):
         return MDmisctsets()
