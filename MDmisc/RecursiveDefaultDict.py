fb280f71363bde074fd9d1f95bbdeddafe96bc7f
Marco De Donno <Marco.DeDonno@unil.ch>
2017-01-02 00:13:19 +0100

    Add the edefaultdict.to_dict() function

--- MDmisc/RecursiveDefaultDict.py
+++ MDmisc/RecursiveDefaultDict.py
@@ -9,5 +9,12 @@ class edefaultdict( defaultdict ):
     def get_by_keys( self, lst ):
         return [ self.get( key ) for key in lst ]
 
+    def to_dict( self ):
+        for key, value in self.iteritems():
+            if isinstance( value, edefaultdict ):
+                self[ key ] = value.to_dict()
+            
+        return dict( self )
+
 defDict = lambda: edefaultdict( defDict )
 defOrderedDict = lambda: DefaultOrderedDict( defOrderedDict )
