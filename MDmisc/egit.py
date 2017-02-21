b887aecca72432d5cbcf8fc1a47748d567a77db1
Marco De Donno <Marco.DeDonno@unil.ch>
2017-02-21 18:17:17 +0100

    Add support to process ModuleType directly

--- MDmisc/egit.py
+++ MDmisc/egit.py
@@ -3,35 +3,40 @@
 
 import git
 import re
+from types import ModuleType
 
 def git_version( wd = "./", name = None ):
-    try:
-        repo = git.Repo( wd, search_parent_directories = True )
-        
-    except git.exc.NoSuchPathError:
-        return {}
+    if isinstance( wd, ModuleType ):
+        return git_version( wd.__path__[ 0 ], wd.__name__ )
     
     else:
         try:
-            describe = repo.git.describe( "--dirty" )
-            commit_count = int( repo.git.rev_list( "--count", "HEAD" ) )
+            repo = git.Repo( wd, search_parent_directories = True )
+            
+        except git.exc.NoSuchPathError:
+            return {}
         
-        except:
-            describe = None
-            commit_count = None
-        
-        finally:
-            hash = repo.head.object.hexsha
-            date = repo.git.log( "-1", "--date=iso", "--pretty=format:%cd" )
-             
-            return {
-                'name': name,
-                'working_dir': repo.working_dir.replace( "\\", "/" ),
-                'commit_describe': describe,
-                'commit_count': commit_count,
-                'commit_hash': hash,
-                'commit_date': date
-            }
+        else:
+            try:
+                describe = repo.git.describe( "--dirty" )
+                commit_count = int( repo.git.rev_list( "--count", "HEAD" ) )
+            
+            except:
+                describe = None
+                commit_count = None
+            
+            finally:
+                hash = repo.head.object.hexsha
+                date = repo.git.log( "-1", "--date=iso", "--pretty=format:%cd" )
+                 
+                return {
+                    'name': name,
+                    'working_dir': repo.working_dir.replace( "\\", "/" ),
+                    'commit_describe': describe,
+                    'commit_count': commit_count,
+                    'commit_hash': hash,
+                    'commit_date': date
+                }
 
 def git_describe_split( describe ):
     try:
