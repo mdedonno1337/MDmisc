16f5918252ee07aee587e56b9ad92c4d94f4c23b
Marco De Donno <Marco.DeDonno@unil.ch>
2017-04-21 17:24:10 +0200

    Update the fuckit module to version 4.8.0

--- MDmisc/fuckit.py
+++ MDmisc/fuckit.py
@@ -1,3 +1,23 @@
+"""
+https://github.com/ajalt/fuckitpy
+
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                   Version 2, December 2004
+
+Copyright (C) 2014-2017 AJ Alt
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+0. You just DO WHAT THE FUCK YOU WANT TO."""
+
+from _hashlib import new
+
 __doc__ = """Steamroll errors.
 
 Getting import errors? Use the fuckit function as a replacement for import if an
@@ -57,37 +77,37 @@ import ast
 import sys
 import types
 
-class _fuckit(types.ModuleType):
+class _fuckit( types.ModuleType ):
     # We overwrite the sys.modules entry for this function later, which will
     # cause all the values in globals() to be changed to None to allow garbage
     # collection. That forces us to do all of our imports into locals().
-    class _Fucker(ast.NodeTransformer):
+    class _Fucker( ast.NodeTransformer ):
         """Surround each statement with a try/except block to silence errors."""
-        def generic_visit(self, node):
+        def generic_visit( self, node ):
             import ast
             import sys
-            ast.NodeTransformer.generic_visit(self, node)
+            ast.NodeTransformer.generic_visit( self, node )
     
-            if isinstance(node, ast.stmt) and not isinstance(node, ast.FunctionDef):
+            if isinstance( node, ast.stmt ) and not isinstance( node, ast.FunctionDef ):
                 if sys.version_info[0] == 3:
-                    new_node = ast.Try(
-                        body=[node],
-                        handlers=[ast.ExceptHandler(type=None,
-                                                    name=None,
-                                                    body=[ast.Pass()])],
-                        orelse=[],
-                        finalbody=[ast.Pass()])
+                    new_node = ast.Try( 
+                        body = [node],
+                        handlers = [ast.ExceptHandler( type = None,
+                                                    name = None,
+                                                    body = [ast.Pass()] )],
+                        orelse = [],
+                        finalbody = [ast.Pass()] )
                 else:
-                    new_node = ast.TryExcept(
-                        body=[node],
-                        handlers=[ast.ExceptHandler(type=None,
-                                                    name=None,
-                                                    body=[ast.Pass()])],
-                        orelse=[])
-                return ast.copy_location(new_node, node)
+                    new_node = ast.TryExcept( 
+                        body = [node],
+                        handlers = [ast.ExceptHandler( type = None,
+                                                    name = None,
+                                                    body = [ast.Pass()] )],
+                        orelse = [] )
+                return ast.copy_location( new_node, node )
             return node
     
-    def __call__(self, victim):
+    def __call__( self, victim ):
         """Steamroll errors.
     
         The argument can be the string name of a module to import, an existing
@@ -107,85 +127,97 @@ class _fuckit(types.ModuleType):
             basestring = str
             get_func_code = lambda f: f.__code__
             exec_ = __builtins__['exec']
+            types.ClassType = type
         else:
             basestring = __builtins__['basestring']
             get_func_code = lambda f: f.func_code
-            def exec_(_code_, _globs_):
+            def exec_( _code_, _globs_ ):
                 _locs_ = _globs_
-                exec('exec _code_ in _globs_, _locs_')
+                exec( 'exec _code_ in _globs_, _locs_' )
 
-        if isinstance(victim, basestring):
-            sourcefile, pathname, _description = imp.find_module(victim)
-            source = sourcefile.read()
-            # Compile the module with more and more lines removed until it
-            # imports successfully.
-            while True:
+        if isinstance( victim, basestring ):
+            sourcefile, pathname, ( _, _, module_type ) = imp.find_module( victim )
+            if module_type == imp.PY_SOURCE:
+                source = sourcefile.read()
+                # If we have the source, we can silence SyntaxErrors by
+                # compiling the module with more and more lines removed until
+                # it imports successfully.
+                while True:
+                    try:
+                        code = compile( source, pathname, 'exec' )
+                        module = types.ModuleType( victim )
+                        module.__file__ = pathname
+                        sys.modules[victim] = module
+                        exec_( code, module.__dict__ )
+                    except Exception as exc:
+                        extracted_ln = traceback.extract_tb( sys.exc_info()[2] )[-1][1]
+                        lineno = getattr( exc, 'lineno', extracted_ln )
+                        lines = source.splitlines()
+                        del lines[lineno - 1]
+                        source = '\n'.join( lines )
+                        if not PY3:
+                            source < -True # Dereference assignment to fix truthiness in Py2
+                    else:
+                        break
+            else:
+                # If we don't have access to the source code, there's not much
+                # we can do to stop import-time errors.
                 try:
-                    code = compile(source, pathname, 'exec')
-                    module = types.ModuleType(victim)
-                    module.__file__ = pathname
-                    sys.modules[victim] = module
-                    exec_(code, module.__dict__)
-                except Exception as exc:
-                    extracted_ln = traceback.extract_tb(sys.exc_info()[2])[-1][1]
-                    lineno = getattr(exc, 'lineno', extracted_ln)
-                    lines = source.splitlines()
-                    del lines[lineno - 1]
-                    source = '\n'.join(lines)
-                    if not PY3:
-                        source <- True # Dereference assignment to fix truthiness in Py2
-                else:
-                    break
+                    module = __import__( victim )
+                except Exception:
+                    # If the module doesn't import at this point, it's
+                    # obviously not worth using anyway, so just return an
+                    # empty module.
+                    module = types.ModuleType( victim )
             inspect.stack()[1][0].f_locals[victim] = module
             return module
-        elif inspect.isfunction(victim) or inspect.ismethod(victim):
+        elif inspect.isfunction( victim ) or inspect.ismethod( victim ):
             try:
-                sourcelines = inspect.getsource(get_func_code(victim)).splitlines()
-                indent = re.match(r'\s*', sourcelines[0]).group()
-                source = '\n'.join(l.replace(indent, '', 1) for l in sourcelines)
+                sourcelines = inspect.getsource( get_func_code( victim ) ).splitlines()
+                indent = re.match( r'\s*', sourcelines[0] ).group()
+                source = '\n'.join( l.replace( indent, '', 1 ) for l in sourcelines )
             except IOError:
                 # Worst-case scenario we can only catch errors at a granularity
                 # of the whole function.
-                @functools.wraps(victim)
-                def wrapper(*args, **kw):
+                @functools.wraps( victim )
+                def wrapper( *args, **kw ):
                     try:
-                        victim(*args, **kw)
+                        victim( *args, **kw )
                     except Exception:
                         pass
                 return wrapper
             else:
                 # If we have access to the source, we can silence errors on a
                 # per-expression basis, which is "better".
-                tree = self._Fucker().visit(ast.parse(source))
+                tree = self._Fucker().visit( ast.parse( source ) )
                 del tree.body[0].decorator_list[:]
-                ast.fix_missing_locations(tree)
-                code = compile(tree, victim.__name__, 'exec')
-                namespace = {}
-                exec_(code, namespace)
+                ast.fix_missing_locations( tree )
+                code = compile( tree, victim.__name__, 'exec' )
+                namespace = dict( victim.__globals__ )
+                exec_( code, namespace )
                 return namespace[victim.__name__]
-        elif isinstance(victim, types.ModuleType):
+        elif isinstance( victim, types.ModuleType ):
             # Allow chaining of fuckit import calls
             for name, obj in victim.__dict__.items():
-                if inspect.isfunction(obj) or inspect.ismethod(obj):
-                    victim.__dict__[name] = self(obj)
+                if inspect.isfunction( obj ) or inspect.ismethod( obj ):
+                    victim.__dict__[name] = self( obj )
             return victim
-        elif isinstance(victim, (types.ClassType, type)):
+        elif isinstance( victim, ( types.ClassType, type ) ):
             for name, member in victim.__dict__.items():
-                if isinstance(member, (type, types.ClassType, types.FunctionType,
-                                       types.LambdaType, types.MethodType)):
-                    setattr(victim, name, self(member))
+                if isinstance( member, ( type, types.ClassType, types.FunctionType,
+                                       types.LambdaType, types.MethodType ) ):
+                    setattr( victim, name, self( member ) )
             return victim
     
         return victim
     
-    def __enter__(self):
+    def __enter__( self ):
         return None
     
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__( self, exc_type, exc_value, traceback ):
         # Returning True prevents the error from propagating. Don't silence
         # KeyboardInterrupt or SystemExit. We aren't monsters.
-        return exc_type is None or issubclass(exc_type, Exception)
+        return exc_type is None or issubclass( exc_type, Exception )
     
     
-sys.modules[__name__] = _fuckit('fuckit', __doc__)
-    
+sys.modules[__name__] = _fuckit( 'fuckit', __doc__ )
