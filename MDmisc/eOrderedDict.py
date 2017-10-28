f7d2c41889d1dd618274782a499f5cbfedbb63ba
Marco De Donno <Marco.DeDonno@unil.ch>
2017-10-28 14:48:46 +0200

    Add the eOrderedDict module

---
+++ MDmisc/eOrderedDict.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from collections import OrderedDict
+
+class eOrderedDict( OrderedDict ):
+    def get_r( self, path, default = None, sep = "/" ):
+        ret = self
+        try:
+            for key in path.split( sep ):
+                ret = ret.get( key )
+            return ret
+        
+        except:
+            return default
+
+def eOrderedDictParser( obj ):
+    if isinstance( obj, OrderedDict ):
+        obj.__class__ = eOrderedDict
+        
+        for i, ( k, v ) in enumerate( obj.iteritems() ):
+            eOrderedDictParser( v )
+    
+    return obj
