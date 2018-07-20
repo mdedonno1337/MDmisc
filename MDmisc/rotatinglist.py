e53bcff3532294173b1e3c6674cceec04ff230b2
Marco De Donno <Marco.DeDonno@unil.ch>
2018-07-20 23:05:34 +0200

    Add functions to the rotating_list object

--- MDmisc/rotatinglist.py
+++ MDmisc/rotatinglist.py
@@ -9,6 +9,12 @@ class rotating_list( object ):
             self.data = data
         self.i = 0
     
+    def current( self ):
+        return self.data[ self.i ]
+    
+    def last( self ):
+        return self.data[ -1 ]
+    
     def append( self, el ):
         return self.data.append( el )
     
@@ -16,3 +22,14 @@ class rotating_list( object ):
         d = self.data[ self.i ]
         self.i = ( self.i + 1 ) % len( self.data )
         return d
+    
+    def rnext( self ):
+        n = self.next()
+        if isinstance( n, rotating_list ):
+            return n.next()
+        else:
+            return n
+    
+    def __repr__( self, *args, **kwargs ):
+        return str( self.data )
+    
