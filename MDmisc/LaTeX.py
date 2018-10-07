6058a335e3b021ae0662f8adb20b37f6dc30e066
Marco De Donno <Marco.DeDonno@unil.ch>
2018-10-07 19:51:08 +0200

    Add support for Engineering notation for the LaTeX_scientific_notation() function

--- MDmisc/LaTeX.py
+++ MDmisc/LaTeX.py
@@ -4,7 +4,7 @@
 from MDmisc.numbers import frexp10
 
 
-def LaTeX_scientific_notation( nb, digits = 5 ):
+def LaTeX_scientific_notation( nb, digits = 5, eng = False ):
     """
         Convert the scientific notation of python () to the 'traditionnal'
         annotation.
@@ -14,4 +14,11 @@ def LaTeX_scientific_notation( nb, digits = 5 ):
     """
     mantisse, exp = frexp10( nb )
     sign = '+' if exp > 0 else ''
+    
+    if eng:
+        expdiff = exp % 3
+        if expdiff != 0:
+            mantisse *= pow( 10, expdiff )
+            exp -= expdiff
+    
     return ( r"$%%.%df \times 10^{%%s%%d}$" % digits ) % ( mantisse, sign, exp )
