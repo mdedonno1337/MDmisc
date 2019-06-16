413bce449cfd6596727de7c07d69b1d3cd2f7846
Marco De Donno <Marco.DeDonno@unil.ch>
2019-06-16 02:55:36 +0200

    Add the function to create a database

--- MDmisc/database.py
+++ MDmisc/database.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import psycopg2
-import psycopg2.extras
 import re
+
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import psycopg2.extras
 
 def urlsplit( url ):
     data = re.match( '((?P<protocol>[^:]+)://)((?P<user>[^:]+)?(:(?P<password>[^@]+))?)?@(?P<host>[^:/]+)(:(?P<port>\d+))?(/(?P<database>[^&]+))?', url )
@@ -57,3 +58,13 @@ class Database( object ):
     def __exit__( self, exc_type, exc_val, exc_tb ):
         self.commit()
         self.close()
+
+def create_database( url, base ):
+    with Database( url ) as db:
+        db.conn.set_isolation_level( ISOLATION_LEVEL_AUTOCOMMIT )
+        c = db.conn.cursor()
+        
+        c.execute( "CREATE DATABASE %s" % base )
+        
+        sql = "SELECT count( datname ) as nb FROM pg_catalog.pg_database WHERE datname = %s"
+        return db.query_fetchone( sql, ( base, ) )[ 'nb' ] == 1
